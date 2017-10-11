7de6b5c53aa48f951a29d2e1ed47b4d6556d0ed3
Jeroen Hammann <jeroenhammann@gmail.com>
2017-10-11 15:15:56 +0200

    Rename files.

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+// Rename a large number of files. Change the files into a numbered list based on the original order determined by user input (in input.js).
+const gulp = require('gulp');
+const gulpLoadPlugins = require('gulp-load-plugins');
+const files = require('./input');
+
+const $ = gulpLoadPlugins();
+
+gulp.task('default', () => {
+  files.forEach((file, index) => {
+    const newName = index + 1;
+    const extension = file.split('.').pop();
+    gulp.src(`./files/${file}`)
+      .pipe($.rename(`${newName}.${extension}`))
+      .pipe(gulp.dest('./new/'));
+  });
+});
